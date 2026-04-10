c3cf441d461689ecac9eb89af85b98751d6391ff
Somnath Nandi <somnathnandi368@gmail.com>
2026-04-10 12:07:55 +0530

    adding missing function body

--- include/smem/mem.h
+++ include/smem/mem.h
@@ -11,7 +11,12 @@
  * @param size Number of bytes to allocate.
  * @return Pointer to the allocated memory, or NULL if allocation fails.
  */
-void *smalloc(size_t size);
+void *smalloc(size_t size)
+{
+    void *mem = malloc(size);
+    smpool_append(mem);
+    return mem;
+}
 
 /**
  * @brief Reallocates memory and updates the global pool.
@@ -22,7 +27,13 @@ void *smalloc(size_t size);
  * @param size New size in bytes.
  * @return Pointer to the reallocated memory, or NULL if reallocation fails.
  */
-void *smrealloc(void *ptr, size_t size);
+void *smrealloc(void *ptr, size_t size)
+{
+    void *mem = realloc(ptr, size);
+    smpool_append(mem);
+    smpool_remove(ptr);
+    return mem;
+}
 
 /**
  * @brief Frees memory and removes it from the global pool.
@@ -31,7 +42,11 @@ void *smrealloc(void *ptr, size_t size);
  *
  * @param ptr Pointer to the memory to free.
  */
-void smfree(void *ptr);
+void smfree(void *ptr)
+{
+    free(ptr);
+    smpool_remove(ptr);
+}
 
 /**
  * @brief Allocates and initializes memory to zero, adding it to the global pool.
@@ -42,4 +57,9 @@ void smfree(void *ptr);
  * @param len Number of elements.
  * @return Pointer to the allocated memory, or NULL if allocation fails.
  */
-void *smcalloc(size_t size, size_t len);
+void *smcalloc(size_t size, size_t len)
+{
+    void *mem = calloc(size, len);
+    smpool_append(mem);
+    return mem;
+}
--- include/smem/pool.h
+++ include/smem/pool.h
@@ -41,7 +41,16 @@ static smpool * smem_gpool = NULL;
  * Allocates and initializes a new smpool structure and sets it as the global pool.
  * The pool starts with a capacity of 10 addresses.
  */
-void smpool_init();
+void smpool_init()
+{
+    smpool *pool = (smpool *)malloc(sizeof(smpool));
+    pool->length = 10;
+    pool->addr = (uintptr_t *)calloc(pool->length, sizeof(uintptr_t));
+    pool->count = 0;
+    pool->allocated = 0;
+    pool->freed = 0;
+    smem_gpool = pool;
+}
 
 /**
  * @brief Destroys the global shared memory pool.
@@ -49,7 +58,20 @@ void smpool_init();
  * Frees all memory addresses stored in the pool and then frees the pool structure itself.
  * After calling this, the global pool is no longer valid.
  */
-void smpool_destroy();
+void smpool_destroy()
+{
+    smpool* pool = smem_gpool;
+    for (int i = 0; i < pool->count; i++)
+    {
+        if (pool->addr[i] != 0x0)
+        {
+            free((void *)pool->addr[i]);
+            pool->addr[i] = 0;
+        }
+    }
+    free(pool->addr);
+    free(pool);
+}
 
 /**
  * @brief Adds a memory pointer to the global pool.
@@ -57,7 +79,22 @@ void smpool_destroy();
  * @param ptr Pointer to the allocated memory to add to the pool.
  * If the pool is full, its capacity is increased by 10.
  */
-void smpool_append(void *ptr);
+void smpool_append(void *ptr)
+{
+    smpool *pool = smem_gpool;
+    if (pool->count >= pool->length)
+    {
+        pool->length += 10;
+        pool->addr = (uintptr_t *)realloc(pool->addr, pool->length * sizeof(uintptr_t));
+        for (int i = pool->length - 10; i < pool->length; i++)
+        {
+            pool->addr[i] = 0;
+        }
+    }
+    pool->addr[pool->count] = (uintptr_t)ptr;
+    pool->count++;
+    pool->allocated++;
+}
 
 /**
  * @brief Removes a memory pointer from the global pool.
@@ -65,4 +102,21 @@ void smpool_append(void *ptr);
  * @param ptr Pointer to the memory to remove from the pool.
  * The pointer is found and removed, with the last element moved to its position if not at the end.
  */
-void smpool_remove(void *ptr);
+void smpool_remove(void *ptr)
+{
+    smpool *pool = smem_gpool;
+    for (int i = 0; i < pool->count; i++)
+    {
+        if (pool->addr[i] == (uintptr_t)ptr)
+        {
+            pool->addr[i] = 0;
+            if (i != pool->count-1)
+            {
+                pool->addr[i] = pool->addr[pool->count - 1];
+                pool->addr[pool->count - 1] = 0;
+            }
+            pool->count--;
+        }
+    }
+    pool->freed++;
+}
